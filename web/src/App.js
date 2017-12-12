3517c515609604904f0df91f753ad592b25150e3
IsNology <glenn@isnology.com>
2017-12-12 14:36:57 +1100

    add routes and authentication to app

--- web/src/App.js
+++ web/src/App.js
@@ -1,10 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
 import SignInForm from './components/SignInForm'
 import SignUpForm from './components/SignUpForm'
 import ProductList from './components/ProductList'
 import ProductForm from './components/ProductForm'
 import Wishlist from './components/Wishlist'
+import PrimaryNav from './components/PrimaryNav'
 import { signIn, signUp, signOutNow } from './api/auth'
 import { getDecodedToken } from './api/token'
 import { listProducts, createProduct, updateProduct } from './api/products'
@@ -94,68 +96,108 @@ class App extends Component {
     const { decodedToken, products, editedProductID, wishlist } = this.state
     const signedIn = !!decodedToken
 
-    return (
-      <div className="App">
-        <h1>Yarra</h1>
-        <h2 className='mb-3'>Now Delivering: Shipping trillions of new products</h2>
-        {
-          signedIn ? (
-            <div className='mb-3'>
-              <p>Email: { decodedToken.email }</p>
-              <p>Signed in at: { new Date(decodedToken.iat * 1000).toISOString() }</p>
-              <p>Expire at: { new Date(decodedToken.exp * 1000).toISOString() }</p>
-              <button onClick={ this.onSignOut }>
-                Sign Out
-              </button>
-            </div>
-          ) : (
-            <div>
-              <h2>Sign In</h2>
-              <SignInForm
-                onSignIn={ this.onSignIn }
-              />
+    const requireAuth = (render) =>  (props) => (
+      signedIn ? (
+          render(props)
+      ) : (
+        <Redirect to='/signin'  />
+      )
+    )
 
-              <h2>Sign Up</h2>
-              <SignUpForm
-                onSignUp={ this.onSignUp }
-              />
-            </div>
-          )
-        }
-        { products &&
-          <ProductList
-            products={ products }
-            editedProductID={ editedProductID }
-            onEditProduct={ this.onBeginEditingProduct }
-            onAddProductToWishlist={ this.onAddProductToWishlist }
-            onRemoveProductFromWishlist={ this.onRemoveProductFromWishlist }
-            renderEditForm={ (product) => (
-              <div className='ml-3'>
-                <ProductForm
-                  initialProduct={ product }
-                  submitTitle='Update Product'
-                  onSubmit={ this.onUpdateEditedProduct }
+
+
+    return (
+      <Router>
+        <div className="App">
+          <PrimaryNav signedIn={ signedIn } />
+
+          <Route path='/' exact render= { () => (
+            <Fragment>
+              <h1>Yarra</h1>
+              <h2 className='mb-3'>Now Delivering: Shipping trillions of new products</h2>
+            </Fragment>
+            )} />
+
+          <Route path='/signin' exact render= { () => (
+              <Fragment>
+                <h2>Sign In</h2>
+                <SignInForm
+                    onSignIn={ this.onSignIn }
                 />
+              </Fragment>
+          )} />
+
+          <Route path='/signup' exact render= { () => (
+              <Fragment>
+                <h2>Sign Up</h2>
+                <SignUpForm
+                    onSignUp={ this.onSignUp }
+                />
+              </Fragment>
+          )} />
+
+          <Route path='/account' exact render= { requireAuth(() => (
+            <Fragment>
+              <div className='mb-3'>
+                <p>Email: { decodedToken.email }</p>
+                <p>Signed in at: { new Date(decodedToken.iat * 1000).toISOString() }</p>
+                <p>Expire at: { new Date(decodedToken.exp * 1000).toISOString() }</p>
+                <button onClick={ this.onSignOut }>
+                  Sign Out
+                </button>
               </div>
-            ) }
-          />
-        }
-        { signedIn &&
-          <div className='mb-3'>
-            <h2>Create Product</h2>
-            <ProductForm
-              submitTitle='Create Product'
-              onSubmit={ this.onCreateProduct }
-            />
-          </div>
-        }
-        { signedIn && wishlist &&
-          <Wishlist
-            products={ wishlist.products }
-            onRemoveProductFromWishlist={ this.onRemoveProductFromWishlist }
-          />
-        }
-      </div>
+            </Fragment>
+          ))} />
+
+          <Route path='/products' exact render= { requireAuth(() => (
+            <Fragment>
+              { products &&
+                  <ProductList
+                  products={ products }
+                  editedProductID={ editedProductID }
+                  onEditProduct={ this.onBeginEditingProduct }
+                  onAddProductToWishlist={ this.onAddProductToWishlist }
+                  onRemoveProductFromWishlist={ this.onRemoveProductFromWishlist }
+                  renderEditForm={ (product) => (
+                    <div className='ml-3'>
+                      <ProductForm
+                        initialProduct={ product }
+                        submitTitle='Update Product'
+                        onSubmit={ this.onUpdateEditedProduct }
+                      />
+                    </div>
+                  ) }
+                />
+              }
+            </Fragment>
+          ))} />
+
+          <Route path='/admin/products' exact render= { requireAuth(() => (
+            <Fragment>
+              { signedIn &&
+                <div className='mb-3'>
+                  <h2>Create Product</h2>
+                  <ProductForm
+                    submitTitle='Create Product'
+                    onSubmit={ this.onCreateProduct }
+                  />
+                </div>
+              }
+            </Fragment>
+          ))} />
+
+          <Route path='/wishlist' exact render= { requireAuth(() => (
+            <Fragment>
+              { wishlist &&
+                <Wishlist
+                  products={ wishlist.products }
+                  onRemoveProductFromWishlist={ this.onRemoveProductFromWishlist }
+                />
+              }
+            </Fragment>
+          ))} />
+        </div>
+      </Router>
     );
   }
 
@@ -169,7 +211,7 @@ class App extends Component {
         .catch((error) => {
           console.error('error loading products', error)
         })
-      
+
       listWishlist()
         .then((wishlist) => {
           this.setState({ wishlist })
